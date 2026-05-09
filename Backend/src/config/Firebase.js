7dfe55110dd930aa2d75836f4f235508c533685c
Altanshagai <altanshagai@Altanshagais-MacBook-Air.local>
2026-05-10 00:20:59 +0800

    students burtgel

--- Backend/src/config/Firebase.js
+++ Backend/src/config/Firebase.js
@@ -1,9 +1,13 @@
+// src/config/firebase.js
+
 const admin = require("firebase-admin");
 const serviceAccount = require("../../serviceAccountKey.json");
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
 
 const db = admin.firestore();
 
